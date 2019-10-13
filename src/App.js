8b9e3918a6f6df620ca4599c2556a2f58f1aa6b1
Abdul Ghaffar <aghaffar570@gmail.com>
2019-10-12 22:59:23 -0400

    create letter bank

--- src/App.js
+++ src/App.js
@@ -1,10 +1,63 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from 'react';
+import styled from 'styled-components';
 
-function App() {
+
+const GameTitle = styled.h1`
+  font-family: 'Mansalva', cursive;
+  text-transform: uppercase;
+  text-align: center;
+`
+
+const Letter = styled.span`
+  margin: 10px;
+  font-size: 24px;
+  font-weight: 500;
+  font-family: 'Mansalva', cursive;
+  text-transform: uppercase;
+  cursor: pointer;
+
+  &:hover {
+    color: lightcoral;
+  }
+
+  &[disabled] {
+    color: grey;
+    opacity: 0.4;
+    pointer-events: none;
+    text-decoration: line-through;
+  }
+`
+
+const LetterBank = styled.div`
+  width: 60%;
+  height: 50%;
+  padding: 15px;
+  margin: 0 auto;
+  display: flex;
+  flex-wrap: wrap;
+  background-color: lavenderblush;
+`;
+
+const alphabets = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+const App = () => {
+  const [guesses, setGuesses] = useState([]);
+
   return (
-    <div className="App">
-      <h1>Hangman</h1>
+    <div className='App'>
+      <GameTitle>Hangman</GameTitle>
+      <LetterBank>
+        {
+          alphabets.map((char, idx) =>
+            <Letter
+              key={idx}
+              disabled={guesses.includes(char)}
+              onClick={() => setGuesses([...guesses, char])}
+            >
+              {char}
+            </Letter>
+        )}
+      </LetterBank>
     </div>
   );
 }
